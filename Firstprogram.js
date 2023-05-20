ee34d6053925299d4a9a44843553cf9c2a19d5df
dehemiweerakoon <dehemiweerakoon@gmail.com>
2023-05-20 11:40:57 +0530

    first commit

---
+++ Firstprogram.js
@@ -0,0 +1,17 @@
+//first program
+let num1=45;
+let num2=89;
+let sum=num1+num2;
+console.log("sum is" +sum);
+
+//second progarm
+let radius=90;
+const pi=3.14;
+let sum2=pi*radius*radius;
+console.log(sum2);
+
+///
+let name="chat bot";
+let year=2023;
+console.log("Hello! My name is "+ name);
+console.log("I was cerated in"+year);
