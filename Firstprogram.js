6d1192e14a2108337a8ec2f03016ddeb9e4deec1
dehemiweerakoon <115975647+dehemiweerakoon@users.noreply.github.com>
2023-05-27 09:50:32 +0530

    fuctions js

--- Firstprogram.js
+++ Firstprogram.js
@@ -1,45 +1,45 @@
 //first program
-let num1=45;
-let num2=89;
-let sum=num1+num2;
-console.log("sum is" +sum);
+let num1 = 45;
+let num2 = 89;
+let sum = num1 + num2;
+console.log("sum is" + sum);
 
 //second progarm
-let radius=90;
-const pi=3.14;
-let sum2=pi*radius*radius;
+let radius = 90;
+const pi = 3.14;
+let sum2 = pi * radius * radius;
 console.log(sum2);
 
 ///
-let name="chat bot";
-let year=2023;
-console.log("Hello! My name is "+ name);
-console.log("I was cerated in"+year);
+let name = "chat bot";
+let year = 2023;
+console.log("Hello! My name is " + name);
+console.log("I was cerated in" + year);
 
 //java Script 
-onsole.log(typeof(6));
+onsole.log(typeof (6));
 
-console.log(typeof(true));
+console.log(typeof (true));
 
-console.log(typeof("Name"));
+console.log(typeof ("Name"));
 
-console.log(typeof("Name"));
+console.log(typeof ("Name"));
 
-console.log("My name is"+"hello");
+console.log("My name is" + "hello");
 
-console.log(10%8);
-
-undefined
-console.log(34%20);
+console.log(10 % 8);
 
 undefined
-console.log(6%5);
+console.log(34 % 20);
 
 undefined
-console.log(2**3);
+console.log(6 % 5);
 
 undefined
-console.log(3**3);
+console.log(2 ** 3);
+
+undefined
+console.log(3 ** 3);
 
 undefined
 //let num1=4;
@@ -58,16 +58,44 @@ let num1=4;
 let num2=3;
 let mod=num1%num2;
 console.log(mod);*/
-console.log(1+(3*4)-2);
+console.log(1 + (3 * 4) - 2);
 
 console.log(-(9));
 
-console.log(-(100+5));
+console.log(-(100 + 5));
 
-console.log("Hello,"+"World!")
+console.log("Hello," + "World!")
 
-console.log((-1)*(-10)*(-20));
+console.log((-1) * (-10) * (-20));
 
-console.log(2*(4+5*(6+3)));
+console.log(2 * (4 + 5 * (6 + 3)));
 
-console.log(36/3+4*3);
+console.log(36 / 3 + 4 * 3);
+
+function rem(num1, num2) {
+    return num1 % num2;
+}
+//add fuction
+function add(num1, num2) {
+    return num1 + num2;
+}
+//sub function
+function sub(num1, num2) {
+    return num1 - num2;
+}
+//multipliction
+function multi(num1, num2) {
+    return num1 * num2;
+}
+//division
+function div(num1, num2) {
+    return num1 / num2;
+}
+//power of the numbers
+function power(num1, num2) {
+    return num1 ** num2;
+}
+//remainder of the numbers
+function remainder(num1, num2) {
+    return num1 % num2;
+}
