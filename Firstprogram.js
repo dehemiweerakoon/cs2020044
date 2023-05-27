b32a282e3be922e71cd997a8d49ad0c05e102592
dehemiweerakoon <115975647+dehemiweerakoon@users.noreply.github.com>
2023-05-27 09:27:33 +0530

    Day 2

--- Firstprogram.js
+++ Firstprogram.js
@@ -15,3 +15,59 @@ let name="chat bot";
 let year=2023;
 console.log("Hello! My name is "+ name);
 console.log("I was cerated in"+year);
+
+//java Script 
+onsole.log(typeof(6));
+
+console.log(typeof(true));
+
+console.log(typeof("Name"));
+
+console.log(typeof("Name"));
+
+console.log("My name is"+"hello");
+
+console.log(10%8);
+
+undefined
+console.log(34%20);
+
+undefined
+console.log(6%5);
+
+undefined
+console.log(2**3);
+
+undefined
+console.log(3**3);
+
+undefined
+//let num1=4;
+//let num2=3;
+//let sum=num1+num2;
+console.log(sum);
+//VM1223:4 7
+/*undefined
+let num1=4;
+let num2=3;
+let exp=num1**num2;
+console.log(exp);
+VM1293:4 64
+undefined
+let num1=4;
+let num2=3;
+let mod=num1%num2;
+console.log(mod);*/
+console.log(1+(3*4)-2);
+
+console.log(-(9));
+
+console.log(-(100+5));
+
+console.log("Hello,"+"World!")
+
+console.log((-1)*(-10)*(-20));
+
+console.log(2*(4+5*(6+3)));
+
+console.log(36/3+4*3);
