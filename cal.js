6e4f288888ace301feee43042005b508418c9949
dehemiweerakoon <115975647+dehemiweerakoon@users.noreply.github.com>
2023-05-27 11:43:12 +0530

    434

---
+++ cal.js
@@ -0,0 +1,6 @@
+function calculate() {
+    let num1=parseInt(document.getElementById("num1"));
+    let num2=parseInt(document.getElementById("num2"));
+    
+    document.getElementById("result").value=  num1+num2;
+ }
